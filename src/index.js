f10606f715b05f5085e2a5f8e4555ba0d6a09cdc
lena <lena6961200@mail.ru>
2019-03-19 16:34:44 +0200

    feat

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
-/**
- * @param preferences - an array of integers. Indices of people, whom they love
- * @returns number of love triangles
+/** * @param preferences - an array of integers. Indices of people, whom they love
+ /** @returns number of love triangles
  */
+
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-};
+  
+  var calc = 0;
+  preferences.unshift(0);
+    for(let i = 1; i < preferences.length; i++){
+        let b = preferences[i];
+        if(b==i) continue;
+        let c = preferences[b];
+        if (c==b) continue;
+        calc += i == preferences[c];
+    }
+    return calc/3;
+}
